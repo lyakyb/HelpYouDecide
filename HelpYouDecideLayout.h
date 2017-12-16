2e7242a7983ec27b8360416c7753299881b412c3
Young Bin Kim <bkim@connectedlab.com>
2017-12-17 03:18:36 +0900

    removed Overlay stuff and changed rollbutton to be a footer of decisionsCollectionVC

---
+++ HelpYouDecideLayout.h
@@ -0,0 +1,13 @@
+//
+//  HelpYouDecideLayout.h
+//  HelpYouDecide
+//
+//  Created by Young Bin Kim on 2017-12-16.
+//  Copyright © 2017 YoungBin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpYouDecideLayout : UICollectionViewFlowLayout
+
+@end
---
+++ HelpYouDecideLayout.m
@@ -0,0 +1,13 @@
+//
+//  HelpYouDecideLayout.m
+//  HelpYouDecide
+//
+//  Created by Young Bin Kim on 2017-12-16.
+//  Copyright © 2017 YoungBin Kim. All rights reserved.
+//
+
+#import "HelpYouDecideLayout.h"
+
+@implementation HelpYouDecideLayout
+
+@end
